e518de4fa4e007a118935c960691da3b5eaa4fb0
gunny6026 <gunny6026@naver.com>
2022-11-16 14:11:56 +0900

    feat : 팔로우 (구독) 기능 모델 설계

---
+++ src/main/java/com/cos/photogramstart/domain/subscribe/Subscribe.java
@@ -0,0 +1,44 @@
+package com.cos.photogramstart.domain.subscribe;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "subscribe_uk",
+                        columnNames = {"fromUserId" ,"toUserId"}
+                )
+        }
+)
+public class Subscribe {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @JoinColumn(name = "fromUserId")
+    @ManyToOne
+    private User fromUser;
+
+    @JoinColumn(name = "toUserId")
+    @ManyToOne
+    private User toUser;
+
+    private LocalDateTime createDate;
+
+    @PrePersist // 디비에 INSERT 되기 직전에 실행
+    public void createDate() {
+        this.createDate = LocalDateTime.now();
+    }
+}
