b6ff027c670a3680c2b61b0cf08e060f1acf5996
gunny6026 <gunny6026@naver.com>
2022-11-19 13:50:15 +0900

    feat : 팔로잉 유저 게시글 렌더링하기

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -9,11 +9,60 @@
 
 // (1) 스토리 로드하기
 function storyLoad() {
-
+	$.ajax({
+		url : `/api/image`,
+		dataType : "json"
+	}).done(res => {
+		console.log("스토리 가져오기" ,res);
+		res.data.forEach(image => {
+			let storyItem = getStoryItem(image);
+			$("#storyList").append(storyItem);
+		});
+	}).fail(error =>{
+		console.log("오류 =",error)
+	})
 }
 
-function getStoryItem() {
+storyLoad();
+function getStoryItem(image) {
+ let item = `<div class="story-list__item">
+			<div class="sl__item__header">
+			<div>
+<img class="profile-image" src="/upload/${image.user.profileImageUrl}" onerror="this.src='/images/person.jpeg'" />
+</div>
+<div>${image.user.username}</div>
+</div>
+<div class="sl__item__img">
+<img src="/upload/${image.postImageUrl}" />
+</div>
+<div class="sl__item__contents">
+<div class="sl__item__contents__icon">
+<button>
+<i class="fas fa-heart active" id="storyLikeIcon-1" onclick="toggleLike()"></i>
+</button>
+</div>
+<span class="like"><b id="storyLikeCount-1">3 </b>likes</span>
+<div class="sl__item__contents__content">
+<p>${image.caption}</p>
+</div>
+<div id="storyCommentList-1">
+<div class="sl__item__contents__comment" id="storyCommentItem-1"">
+<p>
+<b>Lovely :</b> 부럽습니다.
+</p>
+<button>
+<i class="fas fa-times"></i>
+</button>
+</div>
+</div>
+<div class="sl__item__input">
+<input type="text" placeholder="댓글 달기..." id="storyCommentInput-1" />
+<button type="button" onClick="addComment()">게시</button>
+</div>
 
+</div>
+</div>`;
+ return item
 }
 
 // (2) 스토리 스크롤 페이징하기
