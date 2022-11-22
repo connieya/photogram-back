7bc72e5f2261485462afdb5013b6b7343aead412
gunny6026 <gunny6026@naver.com>
2022-11-22 20:24:32 +0900

    feat : 댓글 도메인 생성

---
+++ src/main/java/com/cos/photogramstart/domain/comment/Comment.java
@@ -0,0 +1,38 @@
+package com.cos.photogramstart.domain.comment;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(length = 100 , nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Image image;
+    private LocalDateTime createDate;
+
+    @PrePersist
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+
+}
