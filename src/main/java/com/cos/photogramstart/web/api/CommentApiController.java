6b2e9a7b13b5e2d1b057e44d8fd152d3867c5035
gunny6026 <gunny6026@naver.com>
2022-11-22 20:59:29 +0900

    feat : 댓글 dto 생성

--- src/main/java/com/cos/photogramstart/web/api/CommentApiController.java
+++ src/main/java/com/cos/photogramstart/web/api/CommentApiController.java
@@ -1,12 +1,10 @@
 package com.cos.photogramstart.web.api;
 
 import com.cos.photogramstart.service.CommentService;
+import com.cos.photogramstart.web.dto.comment.CommentDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +13,8 @@ public class CommentApiController {
     private final CommentService commentService;
 
     @PostMapping("/api/comment")
-    public ResponseEntity<?> commentService(){
+    public ResponseEntity<?> commentService(@RequestBody CommentDto commentDto){
+
         return null;
     }
 
---
+++ src/main/java/com/cos/photogramstart/web/dto/comment/CommentDto.java
@@ -0,0 +1,10 @@
+package com.cos.photogramstart.web.dto.comment;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+    private String content;
+    private int imageId;
+
+}
